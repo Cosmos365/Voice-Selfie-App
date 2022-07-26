3bd2cab5c8b251a3981a30a2dce869cc931df788
Cosmos365 <81462673+Cosmos365@users.noreply.github.com>
2022-07-26 18:10:03 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -5,9 +5,28 @@ function start() {
     Recognition.start();
 }
 
+function speak() {
+    var synth = window.speechSynthesis;
+    Speak_data = document.getElementById("textbox").value; 
+    var utterthis = new SpeechSynthesisUtterance(Speak_data);
+    synth.speak(utterthis);
+    Webcam.attach(camera);
+    console.log("yes");
+}
+
 Recognition.onresult = function(event) {
     console.log(event);
     var content = event.results[0][0].transcript;
     console.log(content);
     document.getElementById("textbox").innerHTML = content;
+    speak();
 }
+
+Webcam.set({
+    width: 360,
+    height: 250,
+    image_format: 'png',
+    png_quality: 90
+});
+
+camera = document.getElementById("camera");
