ea06a71c76e2a342994df20fa068ecf524d17029
Cosmos365 <81462673+Cosmos365@users.noreply.github.com>
2022-07-30 18:08:13 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -10,16 +10,24 @@ Recognition.onresult = function(event) {
     var content = event.results[0][0].transcript;
     console.log(content);
     document.getElementById("textbox").innerHTML = content;
-    speak();
+    
+    if(content == "take my selfie") {
+        console.log("taking selfie!");
+        speak();
+    } 
 }
 
 function speak() {
     var synth = window.speechSynthesis;
-    Speak_data = document.getElementById("textbox").value; 
+    Speak_data = "taking your selfie in 5 seconds"; 
     var utterthis = new SpeechSynthesisUtterance(Speak_data);
     synth.speak(utterthis);
     Webcam.attach(camera);
-    console.log("yes");
+
+    setTimeout(function() {
+        take_snapshot();     
+        save();   
+    }, 5000);
 }
 
 Webcam.set({
@@ -30,3 +38,16 @@ Webcam.set({
 });
 
 camera = document.getElementById("camera");
+
+function take_snapshot() {
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'">';
+    });
+}
+
+function save() {
+    link = document.getElementById("link");
+    image = document.getElementById("selfie_image").src;
+    link.href = image;
+    link.click();
+}
